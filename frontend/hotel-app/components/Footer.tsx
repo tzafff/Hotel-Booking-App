2e07f90c3e26672d7b747f7697009bde56d57526
tzafff <christzaf99@gmail.com>
2024-09-01 21:28:31 +0300

    Footer

--- frontend/hotel-app/components/Footer.tsx
+++ frontend/hotel-app/components/Footer.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
+
+const socials = [
+    {
+        icon: <FaInstagram />,
+        href: "#"
+    },
+    {
+        icon: <FaFacebook />,
+        href: "#"
+    },
+    {
+        icon: <FaTwitter />,
+        href: "#"
+    },
+]
 
 const Footer = () => {
     return (
-        <footer className={"bg-cyan-300"}>
-            <div className={"container mx-auto"}>footer</div>
+        <footer className={"bg-primary py-[60px] lg:py-[120px]"}>
+            <div className={"container mx-auto"}>
+                <div className={"flex flex-col lg:flex-row justify-between items-center gap-10"}>
+                    <Link href={"/"}>
+                        <Image
+                            src={"/assets/logo-white.svg"}
+                            width={160}
+                            height={160}
+                            alt={""} />
+                    </Link>
+                    <div className={"flex gap-4"}>
+                        {socials.map((item, index) => {
+                            return (
+                                <Link
+                                    href={item.href}
+                                    key={index}
+                                    className={"bg-accent hover:bg-accent-hover text-white text-lg w-[38px] h-[38px] " +
+                                        "flex items-center justify-center rounded-full transition-all"}
+                                >
+                                    {item.icon}
+                                </Link>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
